1821821ecc32f400fa95c47639357874339a9906
ekokorev <89355519+ekokorev@users.noreply.github.com>
2021-09-27 19:05:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(a[4])
+user_num = int(imput('Number:'))
+a = 1 if user_num > 0 else -1
+print(a)
+
+
+#my_list = [1, 2, 3, 4, 5]
+#for item in my_list:
+    if item % 2 == 1
+    print(item)
+
+
+
+
+
+
+#my_set = {1, 2, 3, 4, 1}
+#print(my_set)
+#my_set.copy = my_set.copy()
+
+
+
+#my_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+#print(my_tuple)
+#print(my_tuple[3])
+#print(my_tuple[:4])
+
+
+
